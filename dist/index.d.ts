8c48ec9ff7f5475103ddf68feac8ed5e90fc641f
Marcio Reis <marcio.reis@outlook.com>
2019-04-16 14:47:55 +0200

    include dist folder

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export declare function padding(str: any, numChars?: number, character?: string): any;
+export declare function paddingLeft(value: string, numChar: number, paddingChar?: string): string;
+export declare function paddingRight(value: string, numChar: number, paddingChar?: string): string;
