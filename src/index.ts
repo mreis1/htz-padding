150acd384f65abd94fd256cae3a412f9521da443
Marcio Reis <marcio.reis@outlook.com>
2019-04-16 12:16:40 +0200

    remove _padding from exports

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,54 @@
-export function padding(str, numChars = 2, character = '0') {
-    numChars = typeof (numChars) === 'undefined' || numChars === null ? numChars = 2 : numChars;
-    while (str.length < padding) {
-        str = character + str;
-    }
-    return str;
+export function padding(str, numChars = 2, character = "0") {
+  numChars =
+    typeof numChars === "undefined" || numChars === null
+      ? (numChars = 2)
+      : numChars;
+  while (str.length < padding) {
+    str = character + str;
+  }
+  return str;
 }
 
-export function _padding(value: string, numChar: number = 2, paddingChar = '0', paddingAt: ('right' | 'left') = 'right') {
-    if (typeof value !== 'string') {
-        throw new Error('Value must be a valid string');
-    }
-    if (typeof paddingChar !== 'string') {
-        throw new Error('paddingChar must be a valid string')
-    }
-    if (typeof numChar !== 'number' || isNaN(numChar)) {
-        throw new Error('Invalid `numChar`');
-    }
+function _padding(
+  value: string,
+  numChar: number = 2,
+  paddingChar = "0",
+  paddingAt: "right" | "left" = "right"
+) {
+  if (typeof value !== "string") {
+    throw new Error("Value must be a valid string");
+  }
+  if (typeof paddingChar !== "string") {
+    throw new Error("paddingChar must be a valid string");
+  }
+  if (typeof numChar !== "number" || isNaN(numChar)) {
+    throw new Error("Invalid `numChar`");
+  }
 
-    if (value === null || value === void 0 || numChar <= 0) {
-        return value;
-    }
-    if (value.length >= numChar) {
-        return value;
-    }
-    while (value.length < numChar) {
-        if (paddingAt === 'right') {
-            value = value + paddingChar;
-        } else {
-            value = paddingChar + value;
-        }
-    }
+  if (value === null || value === void 0 || numChar <= 0) {
     return value;
+  }
+  if (value.length >= numChar) {
+    return value;
+  }
+  while (value.length < numChar) {
+    if (paddingAt === "right") {
+      value = value + paddingChar;
+    } else {
+      value = paddingChar + value;
+    }
+  }
+  return value;
 }
 
-export function paddingLeft(value: string, numChar: number, paddingChar = '0') {
-    return _padding(value, numChar, paddingChar, 'left')
+export function paddingLeft(value: string, numChar: number, paddingChar = "0") {
+  return _padding(value, numChar, paddingChar, "left");
 }
 
-export function paddingRight(value: string, numChar: number, paddingChar = '0') {
-    return _padding(value, numChar, paddingChar, 'right')
+export function paddingRight(
+  value: string,
+  numChar: number,
+  paddingChar = "0"
+) {
+  return _padding(value, numChar, paddingChar, "right");
 }
