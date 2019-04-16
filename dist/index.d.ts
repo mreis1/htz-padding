2e0bb68719e2e4ca8ced5d48990e3b4dc0dfc424
Marcio Reis <marcio.reis@outlook.com>
2019-04-16 14:54:08 +0200

    remove dist

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function padding(str: any, numChars?: number, character?: string): any;
-export declare function paddingLeft(value: string, numChar: number, paddingChar?: string): string;
-export declare function paddingRight(value: string, numChar: number, paddingChar?: string): string;
